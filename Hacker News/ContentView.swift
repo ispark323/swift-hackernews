7d1184618aef3d43bebdcb0cd3434d4090db2b00
Park, Sungmin <ts-sungmin.park@rakuten.com>
2020-03-12 09:48:50 +0900

    Initial Commit

---
+++ Hacker News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hacker News
+//
+//  Created by Park, Sungmin on 2020/03/12.
+//  Copyright © 2020 Sungmin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
